3e98a6d1c2bfff32ac304065dc194af0470d9bd5
CalebCur01 <calebcurry0@gmail.com>
2023-01-23 01:03:51 -0600

    Update listURL3.py

--- listURL3.py
+++ listURL3.py
@@ -1,8 +1,9 @@
-import os, sys, bs4, requests, webbrowser, re
+import os, sys, bs4, requests, webbrowser, re, time
 
-urlList = []
-layerAdded = {}
+urlList = [] #list of website links
+layerAdded = {} #keeps track of what "layer" each link was added
 depth = 3 # how many "layers" before stopping
+maximum = 8 #Set a limit of links to discover before stopping
 
 def urlLister(url,depth):
     if depth == 0:
@@ -14,6 +15,10 @@ def urlLister(url,depth):
         if link not in urlList and type(link) == str:
             urlList.append(link)
             layerAdded.update({link:depth})
+            print("Website link found! - {} - Layers remaining: {}".format(link,depth-1))
+            if len(urlList) >= maximum:
+                print("Limit reached! Quitting early!")
+                return
     for url in urlList:
         if layerAdded.get(url) == depth:
             urlLister(url,depth-1)
@@ -21,12 +26,13 @@ def urlLister(url,depth):
 quit = False
 
 while not quit:
-    url = input("Enter a url: ")
-    urlLister(url,depth)
+    website = input("Enter a url: ")
+    start = time.perf_counter()
+    urlLister(website,depth)
+    stop = time.perf_counter()
 
-    for url in urlList:
-        print(url)
-
+    
+    print("\nStarting from {}, found {} links in {} seconds.".format(website,len(urlList),stop-start))
 
     exitprogram = input("Exit? Y/N?")
     if exitprogram.lower() == 'y':
