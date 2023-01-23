ec0aa66a2fca35d86619da0ca5d8c2b8a6b9f272
CalebCur01 <calebcurry0@gmail.com>
2023-01-22 23:53:02 -0600

    Add files via upload

---
+++ listURL2.py
@@ -0,0 +1,29 @@
+import os, sys, bs4, requests, webbrowser, re
+
+urlList = []
+depth = 1 # how many "layers" before stopping
+
+def urlLister(url,depth):
+    if depth == 0:
+        return
+    req = requests.get(url)
+    soup = bs4.BeautifulSoup(req.text,"html.parser")
+    for links in soup.find_all('a',attrs={'href': re.compile("^http://")}):
+        link = links.get('href')
+        if link not in urlList and type(link) == str:
+            print(url)
+            urlList.append(link)
+    for url in urlList:
+        urlLister(url,depth-1)
+        
+
+url = input("Enter a url: ")
+urlLister(url,depth)
+
+for url in urlList:
+    print(url)
+
+
+exitprogram = input("Exit? Y/N?")
+if exitprogram.lower() == 'n':
+    print("ok")
