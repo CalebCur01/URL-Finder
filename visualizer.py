20b628a784a322d17b3cab3c8275f8e4386e090d
CalebCur01 <calebcurry0@gmail.com>
2023-01-24 01:00:00 -0600

    Update visualizer.py
    slight improvement to readability of graphs

--- visualizer.py
+++ visualizer.py
@@ -3,11 +3,26 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 urlData = json.load(open("urls.txt",'r'))
-data = [(k,v) for k,v in urlData.items()]
+maxUrlSize = 70
+truncatedData = {} #truncate key:values so the urls aren't too long
+
+for k,v in urlData.items():
+    kTrunc = k[:maxUrlSize]
+    vTrunc = v[:maxUrlSize]
+    truncatedData.update({kTrunc:vTrunc})
+
+    
+data = [(k,v) for k,v in truncatedData.items()]
+
+
+
+
 
 Graph = nx.DiGraph()
 Graph.add_edges_from(data)
-nx.draw(Graph,with_labels = True)
+pos = nx.spring_layout(Graph,k=12)
+
+nx.draw(Graph,with_labels= True,node_size=100,font_size=6,arrowsize = 3,alpha = .7,width = .4)
 
 plt.show()
 
