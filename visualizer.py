a56b0e5f92109fe0b6a4d255746d78895931411d
CalebCur01 <calebcurry0@gmail.com>
2023-01-27 13:10:35 -0600

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -2,27 +2,28 @@ import json
 import networkx as nx
 import matplotlib.pyplot as plt
 
-urlData = json.load(open("urls.txt",'r'))
-maxUrlSize = 70
-truncatedData = {} #truncate key:values so the urls aren't too long
 
-for k,v in urlData.items():
-    kTrunc = k[:maxUrlSize]
-    vTrunc = v[:maxUrlSize]
-    truncatedData.update({kTrunc:vTrunc})
+def visualize():
+    urlData = json.load(open("urls.txt",'r'))
+    maxUrlSize = 100
+    truncatedData = {} #truncate key:values so the urls aren't too long
 
-    
-data = [(k,v) for k,v in truncatedData.items()]
+    for k,v in urlData.items():
+        kTrunc = k[:maxUrlSize]
+        vTrunc = v[:maxUrlSize]
+        truncatedData.update({kTrunc:vTrunc})
+
+        
+    data = [(k,v) for k,v in truncatedData.items()]
 
 
 
 
 
-Graph = nx.DiGraph()
-Graph.add_edges_from(data)
-pos = nx.spring_layout(Graph,k=12)
+    Graph = nx.DiGraph()
+    Graph.add_edges_from(data)
+    pos = nx.spring_layout(Graph,k=12)
+    nx.draw(Graph,with_labels= True,node_size=100,font_size=6,arrowsize = 3,alpha = .7,width = .4)
 
-nx.draw(Graph,with_labels= True,node_size=100,font_size=6,arrowsize = 3,alpha = .7,width = .4)
-
-plt.show()
+    plt.show()
 
