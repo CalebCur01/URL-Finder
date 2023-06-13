27fe9434fd7464ad82fc4a3bfe2224199d779490
CalebCur01 <calebcurry0@gmail.com>
2023-06-12 19:49:15 -0500

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -1,13 +1,21 @@
 import json
 import networkx as nx
 import matplotlib.pyplot as plt
+from graphviz import Digraph
+
+def removehttp(string):
+    if(string.find('http://')) >= 0:
+       return string.replace('http://','')
+    if(string.find('https://')) >= 0:
+       return string.replace('https://','')
+    return string
 
 
 def visualize():
     urlData = json.load(open("urls.txt",'r'))
     maxUrlSize = 100
     truncatedData = {} #truncate key:values so the urls aren't too long
-
+    #making the graph for networkX
     for k,v in urlData.items():
         kTrunc = k[:maxUrlSize]
         vTrunc = v[:maxUrlSize]
@@ -17,13 +25,9 @@ def visualize():
     data = [(k,v) for k,v in truncatedData.items()]
 
 
-
-
-
     Graph = nx.DiGraph()
     Graph.add_edges_from(data)
     pos = nx.spring_layout(Graph,k=12)
     nx.draw(Graph,with_labels= True,node_size=100,font_size=6,arrowsize = 3,alpha = .7,width = .4)
 
     plt.show()
-
