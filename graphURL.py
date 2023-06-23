96390e0e80c2d859b283446322b0253db8c8509f
CalebCur01 <calebcurry0@gmail.com>
2023-06-22 21:49:58 -0500

    Update graphURL.py
    updated variable name

--- graphURL.py
+++ graphURL.py
@@ -5,7 +5,7 @@ from visualizer import visualize
 urlList = [] #list of website links
 searched = {} #This will keep track of which webpages have already been searched before
 layerAdded = {} #keeps track of what "layer" each link was added
-parentList = {} #for keeping track of which link came from which website
+parentDict = {} #for keeping track of which link came from which website
 depth = 4 # how many "layers" before stopping
 maximum = 250 #Set a limit of links to discover before stopping
 
@@ -30,7 +30,7 @@ def urlLister(url,depth):
         link = links.get('href')
         if link not in urlList and type(link) == str:
             urlList.append(link)
-            parentList.update({link:url})
+            parentDict.update({link:url})
             layerAdded.update({link:depth})
 
             print("Website link found! - {} - {}".format(link,len(urlList)))
@@ -49,7 +49,7 @@ quit = False
 
 while not quit:
     urlList.clear()
-    parentList.clear()
+    parentDict.clear()
     searched.clear()
     try:
         website = input("Enter a url: ")
@@ -63,7 +63,7 @@ while not quit:
         
         print(f"\nStarting from {website}, found {len(urlList)} links in {stop-start:0.2f} seconds.")
         with open("urls.txt",'w') as file:
-            file.write(json.dumps(parentList))
+            file.write(json.dumps(parentDict))
         visualize()
         
         
