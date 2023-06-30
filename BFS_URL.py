22bb3a0210469a9d3a31aa8e96e141332179a03f
CalebCur01 <calebcurry0@gmail.com>
2023-06-30 09:18:11 -0500

    Update BFS_URL.py
    - normalize URLS to avoid duplicates such as "https://www.foo.com" and "https://.foo.com/"

--- BFS_URL.py
+++ BFS_URL.py
@@ -1,27 +1,43 @@
-#BFS_URL.py - Performs Breadth First Search for webpages
 import os, sys, bs4, requests, webbrowser, re, time, csv
+from urllib.parse import urlparse
 from collections import deque
 from pathlib import Path
 from csv_to_graph import visualize 
 
-data_dict = {} #each key is a parent website, each value is a list of links found on that website
+nodes = {} #each key is a parent website, each value is a list of links found on that website
 max_parents = 0 # how many parents before stopping
 max_children = 0 #how many children before stopping
 child_count = 0 #how many children we have added (includes duplicates)
 
+
+def normalize_url(url):
+    parsed = urlparse(url)
+    scheme = parsed.scheme or 'http'
+    netloc = parsed.netloc or parsed.path
+    path = parsed.path if parsed.netloc else ''
+    if netloc.startswith('www.'):
+        netloc = netloc[4:]
+    if netloc.endswith('/'):
+        netloc = netloc[:-1]
+    if path.endswith('/'):
+        path = path[:-1]
+    return f'{scheme}://{netloc}{path}'
+
 def urlLister(parent, max_parents, max_children):
     """Search for webpages breadth first """
     global child_count
+
+    parent = normalize_url(parent)
 
     queue = deque([(parent)])
 
     while queue:
         parent = queue.popleft()
 
-        if parent in data_dict:
+        if parent in nodes:
             continue
 
-        data_dict[parent] = []
+        nodes[parent] = []
 
         try:
             print(f"Searching {parent} for links...")
@@ -32,13 +48,14 @@ def urlLister(parent, max_parents, max_children):
                 child = children.get('href')
 
                 if type(child) == str:
-                    data_dict[parent].append(child)
+                    child = normalize_url(child)
+                    nodes[parent].append(child)
                     queue.append(child)
                     child_count += 1
 
                     print("Website link found! - {} - {}".format(child,child_count))
 
-                    if child_count >= max_children or len(data_dict) >= max_parents:
+                    if child_count >= max_children or len(nodes) >= max_parents:
                         print("\nLimit reached!")
                         return
 
@@ -64,7 +81,7 @@ def save_csv(data,filename):
 quit = False
 
 while not quit:
-    data_dict.clear()
+    nodes.clear()
     child_count = 0
     parent_count = 0
     try:
@@ -78,7 +95,7 @@ while not quit:
 
         print(f"\nStarting from {website}, found {child_count} links in {stop-start:0.2f} seconds.")
 
-        save_csv(data_dict,"Burls")
+        save_csv(nodes,"Burls")
         visualize("Burls")      
 
     except Exception as e:
